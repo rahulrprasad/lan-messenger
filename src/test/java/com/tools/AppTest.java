e7e9315f27f849986d7c1a0784fb771bb66cb7ba
Rahul R.Prasad <rahulcea@gmail.com>
2015-08-02 16:42:57 +0530

    Adding Travis CI

--- src/test/java/com/tools/AppTest.java
+++ src/test/java/com/tools/AppTest.java
@@ -1,10 +1,15 @@
 package com.tools;
 
+import org.testng.annotations.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    
+    @Test
+    public void test() {
+        System.out.println("Sample test method");
+    }
 }
